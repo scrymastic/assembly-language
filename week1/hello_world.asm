58603498acef9706e2eff952a8e672c06241a9c1
scrymastic <99010732+scrymastic@users.noreply.github.com>
2022-09-11 11:46:39 +0700

    Update hello_world.asm

--- week1/hello_world.asm
+++ week1/hello_world.asm
@@ -2,22 +2,22 @@
 .model small                            ;use small memory model
 
 .data                                   ;data segment
-    message db 'Hello, World!$'         ;string: message ('$' is string termination character)
+    message db 'Hello, World!$'         ;string to be printed ('$' is string termination character)
     
 .code                                   ;code segment
-main proc                               ;main procedure
-    lea ax, data                        ;load data segment address to ax
-    mov ds, ax                          ;ds store value of ax
-  
-    lea dx, message                     ;load message address to dx
+    main proc                           ;main procedure
+        lea ax, data                    ;load data segment address to ax
+        mov ds, ax                      ;ds stores value of ax
 
-    mov ah, 9                           ;these two lines print string indicated by
-    int 21h                             ;ds:dx register
+        lea dx, message                 ;load message address to dx
 
-    mov ah, 4ch                         ;these two lines end the program
-    int 21h                             ;and return the control to the operating system
-    
-main endp
+        mov ah, 9                       ;these two lines print string indicated by ds:dx register
+        int 21h                         ;
+
+        mov ah, 4ch                     ;these two lines end the program and return the control to the operating system
+        int 21h                         ;
+
+    main endp
 end
   
   

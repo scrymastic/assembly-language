52d91ded5a2d5c06d570617c8426fd0252b036a4
scrymastic <99010732+scrymastic@users.noreply.github.com>
2022-10-01 14:45:55 +0700

    Create 1_HelloWorld.asm

---
+++ week1/masm32/1_HelloWorld.asm
@@ -0,0 +1,17 @@
+.386 
+.model flat, stdcall 
+option casemap:none 
+
+
+include \masm32\include\masm32rt.inc
+
+.data
+     msg db "Hello, World!", 0
+
+.code 
+start: 
+    push offset msg
+    call StdOut
+
+    call ExitProcess
+end start
